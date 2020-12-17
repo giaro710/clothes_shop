a44bfd69cad99de79359c6ac887518f9fd1da65d
Gian Marco Romani <giaro710@gmail.com>
2020-12-17 17:11:18 +0100

    Added scss and some basic style configuration

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
+import Header from './components/header/Header';
+import './default.scss';
+
 const App = () => {
   return (
     <div className="App">
+      <Header />
       <h2>Hello world</h2>
     </div>
   );
