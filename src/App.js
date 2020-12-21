7e7fcec6d192574d9d57f5c529b8d97aee38be99
Gian Marco Romani <giaro710@gmail.com>
2020-12-21 14:58:34 +0100

    Added react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
+import React from 'react';
+import { Route } from 'react-router-dom';
 import './default.scss';
 import Header from './components/Header/Header';
-import Homepage from './pages/homepage/Homepage';
+import Homepage from './pages/Homepage/Homepage';
+import Registration from './pages/Registration/Registration';
 
 const App = () => {
   return (
     <div className="app">
       <Header />
       <div className="main">
-        <Homepage />
+        <Route path="/" exact component={Homepage} />
+        <Route path="/registration" exact component={Registration} />
       </div>
     </div>
   );
